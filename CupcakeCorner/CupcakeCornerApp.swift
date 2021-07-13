f72f7c0ae1ccd2514c5319218343cbd37ad8ba58
Andres Marquez <andres@Andreas-Air.hitronhub.home>
2021-07-13 16:26:16 -0600

    Initial commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Andres on 2021-07-11.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
